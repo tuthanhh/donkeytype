1056022bc06fea81068d092626d809293ea08abe
TuThanh <tuthanh10825@gmail.com>
2025-11-03 15:02:34 +0700

    fix: enter tab to restart

--- frontend/src/components/Result.vue
+++ frontend/src/components/Result.vue
@@ -1,22 +1,32 @@
 <script setup>
+import { ref, onMounted } from 'vue'
+
 const props = defineProps({
-  typingResult: {
-    type: Object,
-    required: true,
-  },
+    typingResult: {
+        type: Object,
+        required: true,
+    },
 })
 const emit = defineEmits(['resetTypingResult'])
+
+const resultDiv = ref(null)
+onMounted(() => {
+    // Focus the div to capture keyboard events
+    resultDiv.value?.focus()
+})
 </script>
 <template>
-  <h2>Test Completed!</h2>
-  <p>Your Results:</p>
-  <ul>
-    <li>Start at {{ typingResult.startTime }} to {{ typingResult.endTime }}</li>
-    <li>WPM: {{ typingResult.wpm }}</li>
-    <li>Accuracy: {{ typingResult.accuracy }}%</li>
-    <li>Correct Characters: {{ typingResult.correctChars }}</li>
-    <li>Incorrect Characters: {{ typingResult.incorrectChars }}</li>
-    <li>Total Characters: {{ typingResult.totalChars }}</li>
-  </ul>
-  <button @click="emit('resetTypingResult')">Restart Test</button>
+    <div @keydown.tab="emit('resetTypingResult')" tabindex="0" ref="resultDiv" style="outline: none;">
+        <h2>Test Completed!</h2>
+        <p>Your Results:</p>
+        <ul>
+            <li>Start at {{ props.typingResult.startTime }} to {{ props.typingResult.endTime }}</li>
+            <li>WPM: {{ props.typingResult.wpm }}</li>
+            <li>Accuracy: {{ props.typingResult.accuracy }}%</li>
+            <li>Correct Characters: {{ props.typingResult.correctChars }}</li>
+            <li>Incorrect Characters: {{ props.typingResult.incorrectChars }}</li>
+            <li>Total Characters: {{ props.typingResult.totalChars }}</li>
+        </ul>
+        <button @click="emit('resetTypingResult')">Restart Test</button>
+    </div>
 </template>
